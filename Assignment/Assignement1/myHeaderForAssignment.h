fd41c813c7ab47119c350ab6badd684d1df8a1b5
Bikrant Bikram Pratap Maurya <48064909+bikkimaurya@users.noreply.github.com>
2024-09-06 13:33:57 +0530

    Changes in Probnlem1

---
+++ Assignment/Assignement1/myHeaderForAssignment.h
@@ -0,0 +1,115 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+
+
+const int numYears = 40;//USed to Create  Array of to Store Date
+const int baseYear = 2000;
+using namespace std;
+
+
+//Class is Used to store Data abput property
+//Such as LTCG , Growth Rate
+
+class Property{
+    private :
+        double** data;
+    public:
+
+        Property(){
+            //This Constuctor Reads the CSV file  price-LTCG.csv and stores in data
+            data = new double*[numYears];
+            for (int i = 0; i < numYears; i++) {
+                data[i] = new double[2];  // Pair of LTCG rate and property rate for each year
+            }
+
+            ifstream file("price-inflation.csv");
+            if (!file.is_open()) {
+                throw out_of_range("Error opening file.");
+            }
+
+            string line;
+            int year = baseYear;
+            stringstream ss;
+            getline(file,line);
+            while (std::getline(file, line) ) {
+                stringstream ss(line);
+
+                string input = ss.str();
+                size_t pos1 = input.find(',');
+                size_t pos2 = input.find(',', pos1 + 1);
+                string value1 = input.substr(0, pos1);
+                double grouth = stod(input.substr(pos1 + 1, pos2 - pos1 - 1));
+                double LTCG = stod(input.substr(pos2 + 1));
+                pos1 = value1.find("-");
+                year = stoi(value1.substr(0,pos1));
+
+                // In 2D array at index[][0]  growth rate
+                // and at index[1] LTCG is being stored
+                data[year - baseYear][0] = (grouth);
+                data[year - baseYear][1] = (LTCG);
+
+                // cout<<year << " Grouth Price :"<<grouth<<" LTCG :" <<LTCG<<endl;
+                // year++;
+            }
+        }
+        double getLTCGRate(int sellingYear){
+            //This function takes selling year and return corresponging years LTCGRate
+            return data[sellingYear - baseYear][1];
+        }
+        double getGrowthPrice( int sellingYear){
+            //This function takes selling year and return corrresponding growth rate
+            return data[sellingYear - baseYear][0];
+        }
+};
+
+
+//This class have been is used to inplement effect of LTCG and Growth on property
+class LTCG{
+    private:
+        double purchasedAT;
+        int tillYear;
+        int yearOfPurchase;
+        double ltcg;
+    public:
+        LTCG(double purchasedAt, int year, int purchasingYear){
+            purchasedAT = purchasedAt;
+            tillYear = year;
+            yearOfPurchase = purchasingYear;
+            ltcg = 20;
+
+        }
+
+        double calculateSellingPrice(  Property p) {
+            double sellAt = purchasedAT;
+            // cout<<purchasedAT;
+            int year = tillYear;
+            int current = yearOfPurchase;
+            double temp = 0;
+            while (  current < year) {
+                // cout<<sellAt<< "Year "<< current << " Grouth "<<p.getGrowthPrice(current) << " LTCG "<<p.getLTCGRate(current) <<endl;
+                sellAt*=(1+((p.getGrowthPrice(current) - p.getLTCGRate(current))/100));
+                current+=1;
+            }
+            return sellAt;
+        }
+        double calculateLTCG(Property p){
+            double sellingPrice = calculateSellingPrice(p);
+            if( purchasedAT){
+                double profit = sellingPrice - purchasedAT;
+                profit = profit>0?profit:0;
+                return profit*(ltcg/100);
+            }
+            else {
+                throw "Error";
+            }
+        }
+
+
+};
--- Assignment/Assignement1/problem1.cpp
+++ Assignment/Assignement1/problem1.cpp
@@ -7,102 +7,13 @@
 #include <cmath>
 #include <iomanip>
 
+#include "myHeaderForAssignment.h"
 
-
-const int numYears = 40;//USed to Create  Array of to Store Date
-const int baseYear = 2000;
 using namespace std;
 
 
 //Class is Used to store Data abput property
-//Such as Inflation , Growth Rate
-
-class Property{
-    private :
-        double** data;
-    public:
-
-        Property(){
-            //This Constuctor Reads the CSV file  price-inflation.csv and stores in data
-            data = new double*[numYears];
-            for (int i = 0; i < numYears; i++) {
-                data[i] = new double[2];  // Pair of inflation rate and property rate for each year
-            }
-
-            ifstream file("price-inflation.csv");
-            if (!file.is_open()) {
-                throw out_of_range("Error opening file.");
-            }
-
-            string line;
-            int year = baseYear;
-            stringstream ss;
-            getline(file,line);
-            while (std::getline(file, line) ) {
-                stringstream ss(line);
-
-                string input = ss.str();
-                size_t pos1 = input.find(',');
-                size_t pos2 = input.find(',', pos1 + 1);
-                string value1 = input.substr(0, pos1);
-                double grouth = stod(input.substr(pos1 + 1, pos2 - pos1 - 1));
-                double inflation = stod(input.substr(pos2 + 1));
-                pos1 = value1.find("-");
-                year = stoi(value1.substr(0,pos1));
-
-                // In 2D array at index[][0]  growth rate
-                // and at index[1] Inflation is being stored
-                data[year - baseYear][0] = (grouth);
-                data[year - baseYear][1] = (inflation);
-
-                // cout<<year << " Grouth Price :"<<grouth<<" Inflation :" <<inflation<<endl;
-                // year++;
-            }
-        }
-        double getInflationRate(int sellingYear){
-            //This function takes selling year and return corresponging years InflationRate
-            return data[sellingYear - baseYear][1];
-        }
-        double getGrowthPrice( int sellingYear){
-            //This function takes selling year and return corrresponding growth rate
-            return data[sellingYear - baseYear][0];
-        }
-};
-
-
-//This class have been is used to inplement effect of Inflation and Growth on property
-class Inflation{
-    private:
-        double purchasedAT;
-        int tillYear;
-        int yearOfPurchase;
-    public:
-        Inflation(double purchasedAt, int year, int purchasingYear){
-            purchasedAT = purchasedAt;
-            tillYear = year;
-            yearOfPurchase = purchasingYear;
-        }
-
-        double calculateSellingPrice(  Property p) {
-            //This function takes hostorical data of Propery to calculate selling Price
-
-            double sellAt = purchasedAT;
-            // cout<<purchasedAT;
-            int year = tillYear;
-            int current = yearOfPurchase;
-            double temp = 0;
-            while (  current < year) {
-                // cout<<sellAt<< "Year "<< current << " Grouth "<<p.getGrowthPrice(current) << " Inflation "<<p.getInflationRate(current) <<endl;
-                sellAt*=(1+((p.getGrowthPrice(current) - p.getInflationRate(current))/100));
-                current+=1;
-            }
-            return sellAt;
-        }
-
-
-};
-
-
+//Such as LTCG , Growth Rate
 
 
 
@@ -127,7 +38,7 @@ int main() {
     cout<< "Enter Purchasing Price : ";
     cin>>PurchasingPrice;
     cout<<endl;
-    Inflation inf(PurchasingPrice, yearOfSelling, yearOfPurchase);
-    cout<<"Selling Price for the Propery in Year "<< yearOfSelling<< " be " <<inf.calculateSellingPrice(property)<< "Rs. Only\n";
+    LTCG inf(PurchasingPrice, yearOfSelling, yearOfPurchase);
+    cout<<"Selling Price for the Propery in Year "<< yearOfSelling<< " be " <<inf.calculateSellingPrice(property)<< " Rs. Only\n";
     return 0;
 }
